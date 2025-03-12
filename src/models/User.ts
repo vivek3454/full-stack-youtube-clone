9fec999d30034357ff51a90864bd92a1a2715a97
vivek3454 <vivekparde2411@gmail.com>
2025-03-12 18:55:30 +0530

    database setup

---
+++ src/models/User.ts
@@ -0,0 +1,38 @@
+import mongoose, { model, models, Schema } from "mongoose";
+
+export interface User {
+    _id?: mongoose.Types.ObjectId;
+    clerkId: string;
+    name: string;
+    // bannerUrl: string;
+    imageUrl: string;
+    createdAt?: Date;
+    updatedAt?: Date;
+}
+
+const userSchema = new Schema<User>({
+    clerkId: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+    // bannerUrl: {
+    //     type: String,
+    //     required: true,
+    // },
+    imageUrl: {
+        type: String,
+        required: true,
+    },
+}, {
+    timestamps: true,
+});
+
+userSchema.index({ clerkId: 1 }, { unique: true });
+
+const User = models?.User || model<User>("User",userSchema);
+export default User;
